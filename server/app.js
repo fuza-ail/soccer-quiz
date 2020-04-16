7a003e7b53850770a24aedf594dec38fdcf86513
indrapuji <47031836+indrapuji@users.noreply.github.com>
2020-04-17 00:24:08 +0700

    merge trial ke dev

--- server/app.js
+++ server/app.js
@@ -4,10 +4,32 @@ const app = express()
 const PORT = 3000
 const cors = require('cors')
 const router = require('./routes')
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
 
-app.listen(PORT, () => console.log(`Listening on Port:${PORT}!`))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('created', data => {
+        io.emit('created', 'io emit')
+        socket.broadcast.emit('created', 'broadcast')
+    })
+    socket.on('joining', data => {
+        socket.broadcast.emit('joining', data)
+    })
+    socket.on('choose', data => {
+        socket.broadcast.emit('choose', data)
+    })
+});
+
+http.listen(3002, () => {
+    console.log('listening on *:3000');
+});
